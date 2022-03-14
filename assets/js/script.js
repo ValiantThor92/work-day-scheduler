43b23b12384b2b1577cd43331d1ae38c532d81ff
unknown <travislnelson92@gmail.com>
2022-03-13 19:10:31 -0700

    add load data from local storage for all hours and save button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,28 @@ $(document).ready(function() {
     //assign saveBtn click listener for user input 
     $(".saveBtn").on("click", function() {
 
+        //get nearby values.
+        console.log(this);
+        var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
+
+        // local storage rules
+        localStorage.setItem(time, text);    
+
     })
+
+    //load saved data from LocalStorage - do this for each hour created.
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
 
     function hourTracker() {
 
