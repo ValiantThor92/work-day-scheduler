1419ca1c0c24bfacd7fda772850baa370134761d
unknown <travislnelson92@gmail.com>
2022-03-13 19:01:26 -0700

    add javascript bare js functions to newly created script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    // display current day & time.
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+    //assign saveBtn click listener for user input 
+    $(".saveBtn").on("click", function() {
+
+    })
+
+    function hourTracker() {
+
+    }
+
+    hourTracker();
+})
+
